824caecbfdb23f14ccab6f2f18e327f62b0be78b
daniel-v-e <dvoneschwege@gmail.com>
2022-12-06 02:06:13 +0100

    GPU stress testing scripts

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=/home/dve/Desktop/g2-peg-in-hole/
-export LD_LIBRARY_PATH=/home/dve/anaconda3/envs/smlr1/lib
-echo $PYTHONPATH
-echo $LD_LIBRARY_PATH
-exec $SHELL
+# export LD_LIBRARY_PATH=/home/dve/anaconda3/envs/smlr2/lib
+# echo $PYTHONPATH
+# echo $LD_LIBRARY_PATH
+# exec $SHELL
--- scripts/rl/stableBaselines3/proximalPolicyOptimization.py
+++ scripts/rl/stableBaselines3/proximalPolicyOptimization.py
@@ -17,10 +17,10 @@ from scripts.rl.stableBaselines3.envPanda import envPanda
 print('Check if continuous observation works with PPO')
 
 env_panda   = envPanda()
-# model_panda = PPO("MlpPolicy", env_panda, verbose=1)
-# model_panda.learn(total_timesteps=75000, progress_bar=True)
-# model_panda.save("ppo_panda")
-# del model_panda
+model_panda = PPO("MlpPolicy", env_panda, verbose=1)
+model_panda.learn(total_timesteps=7500, progress_bar=True)
+model_panda.save("ppo_panda")
+del model_panda
 model_panda = PPO.load("ppo_panda")
 obs_panda         = env_panda.reset()
 for i in range(5000):
