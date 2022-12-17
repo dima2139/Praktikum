d097d49688c4dcbf504002053dce0258c805662d
Dmytro Shchurovskyi <ge65zar@mytum.de>
2022-12-17 22:21:59 +0100

    deleted env files from repository

---
+++ envdima.sh
@@ -0,0 +1,5 @@
+export PYTHONPATH=/home/dima/Desktop/Praktikum/g2-peg-in-hole/
+# export LD_LIBRARY_PATH=/home/dve/anaconda3/envs/pt1.13/lib/python3.9/site-packages
+# echo $PYTHONPATH
+# echo $LD_LIBRARY_PATH
+exec $SHELL
